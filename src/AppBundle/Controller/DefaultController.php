9b40933fdcdc02b458bf1739a7c5cce3a5263def
karmazin <karmazzin@ya.ru>
2014-12-04 15:57:13 +0300

    Move view to AppBundle, config Assetic

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,6 +12,8 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        return $this->render(
+          'AppBundle:Default:index.html.twig'
+        );
     }
 }
