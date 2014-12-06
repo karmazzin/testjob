2018fdf64b839b337a65a5280068f72f109fcd94
karmazin <karmazzin@ya.ru>
2014-12-06 12:26:21 +0300

    Rest app create

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,7 +3,12 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use AppBundle\Entity\Product;
 
 class DefaultController extends Controller
 {
@@ -15,5 +20,153 @@ class DefaultController extends Controller
         return $this->render(
           'AppBundle:Default:index.html.twig'
         );
+    }
+
+    /**
+     * @Route(
+     *  "/api/products/{id}",
+     *  requirements={"id": "\d+"},
+     *  defaults={"id": 0}
+     * )
+     * @Method({"DELETE"})
+     */
+    public function deleteProduct(Request $request)
+    {
+        if (!$request->isXmlHttpRequest()) {
+            return $this->redirect('/');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('AppBundle:Product');
+        $product = $repository->find($request->get('id'));
+
+        if (!$product) {
+            throw $this->createNotFoundException('The product does not exist');
+        }
+
+        $em->remove($product);
+        $em->flush();
+
+        return new Response(null, 200);
+
+    }
+
+    /**
+     * @Route(
+     *  "/api/products/{id}",
+     *  requirements={"id": "\d+"},
+     *  defaults={"id": 0}
+     * )
+     * @Method({"GET"})
+     */
+    public function getProductsAction(Request $request) {
+        if (!$request->isXmlHttpRequest()) {
+            return $this->redirect('/');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('AppBundle:Product');
+
+        if ($id = $request->get("id")) {
+            $result = $repository->findById($id);
+        } else {
+            $result = $repository->findAll();
+        }
+
+        if (!$result) {
+            throw $this->createNotFoundException('The product does not exist');
+        }
+
+        $serializer = $this->get('jms_serializer');
+
+        return new Response($serializer->serialize($result, 'json'));
+    }
+
+
+    /**
+     * @Route("/api/products")
+     * @Method({"POST"})
+     */
+    public function addProductAction(Request $request)
+    {
+
+        if (!$request->isXmlHttpRequest()) {
+            return $this->redirect('/');
+        }
+
+
+        $em = $this->getDoctrine()->getManager();
+
+        $product = new Product();
+
+        $product
+          ->setTitle($request->request->get('title'))
+          ->setDescription($request->request->get('description', ''))
+          ->setPhoto($request->files->get('image'));
+
+        $validate_group = ['upload'];
+        $result = $this->validatePersistProduct($product, $validate_group);
+
+        return $result;
+    }
+
+    /**
+     * @Route(
+     *  "/api/products/{id}",
+     *  requirements={"id": "\d+"},
+     *  defaults={"id": 0}
+     * )
+     * @Method({"POST"})
+     */
+    public function updateProductAction(Request $request)
+    {
+        if (!$request->isXmlHttpRequest()) {
+            return $this->redirect('/');
+        }
+
+
+        $em = $this->getDoctrine()->getManager();
+
+        $repository = $em->getRepository('AppBundle:Product');
+        $product = $repository->find($request->get('id'));
+
+        if (!$product) {
+            throw $this->createNotFoundException('The product does not exist');
+        }
+
+        $product
+          ->setTitle($request->request->get('title'))
+          ->setDescription($request->request->get('description', ''));
+
+        if ($photo = $request->files->get('image')) {
+            $product->removePhoto();
+            $product->setPhoto($request->files->get('image'));
+            $validate_group = ['upload'];
+        } else {
+            $validate_group = [];
+        }
+
+        $result = $this->validatePersistProduct($product, $validate_group);
+
+        return $result;
+
+    }
+
+
+    protected function validatePersistProduct($product, $validate_group) {
+
+        $serializer = $this->get('jms_serializer');
+        $validator = $this->get('validator', $validate_group);
+        $errors = $validator->validate($product);
+
+        if (count($errors) > 0) {
+            return new Response($serializer->serialize($errors, 'json'), 400);
+        } else {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($product);
+            $em->flush();
+
+            return new Response($serializer->serialize($product, 'json'), 200);
+        }
     }
 }
--- src/AppBundle/Resources/public/js/controllers.js
+++ src/AppBundle/Resources/public/js/controllers.js
@@ -2,4 +2,99 @@
 
 (function() {
     var app = angular.module('testjob');
+
+    app.factory('Product', function($resource) {
+        return $resource('app_dev.php/api/products/:id', {
+                id: '@id'
+        });
+    });
+
+    app.controller('ModalFormController', function($scope, $modalInstance, $http, product, Product) {
+
+        $scope.product = product;
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+
+        $scope.sendForm = function() {
+            $scope.errors = [];
+
+            Product.save(new Product($scope.product), function(r) {
+                $modalInstance.close(r);
+            }, function(e) {
+                $scope.errors = e.data;
+            });
+        };
+    });
+
+    app.controller('DeleteDialogController', function($scope, $modalInstance, $http, product, Product) {
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+
+        $scope.ok = function() {
+            Product.delete(new Product(product), function(r) {
+                $modalInstance.close(r);
+            }, function(e) {
+                $scope.errors = e.data;
+            });
+        };
+    });
+
+
+
+    app.controller('MainController', function ($scope, $http, $modal, $filter, Product) {
+
+        $scope.products = Product.query();
+
+        $scope.newForm = function() {
+            var modalInstance = $modal.open({
+                templateUrl: '/bundles/app/partial/modalForm.html',
+                controller: 'ModalFormController',
+                resolve: {
+                    product: {}
+                }
+            });
+
+            modalInstance.result.then(function (result) {
+                $scope.products.push(result);
+            });
+        };
+
+        $scope.editForm = function($index) {
+            var modalInstance = $modal.open({
+                templateUrl: '/bundles/app/partial/modalForm.html',
+                controller: 'ModalFormController',
+                resolve: {
+                    product: function () {
+                        return angular.copy($scope.products[$index]);
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (result) {
+                $scope.products.splice($index, 1);
+                $scope.products[$index] = result;
+            });
+        };
+
+        $scope.deleteDialog = function($index) {
+            var modalInstance = $modal.open({
+                templateUrl: '/bundles/app/partial/deleteDialog.html',
+                controller: 'DeleteDialogController',
+                resolve: {
+                    product: function () {
+                        return $scope.products[$index];
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (result) {
+                $scope.$apply($scope.products.splice($index, 1));
+            });
+        };
+
+    });
 })();
