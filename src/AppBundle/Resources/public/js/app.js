a2e52c233684e3398f77654f43afa189f397d008
karmazin <karmazzin@ya.ru>
2014-12-06 12:25:36 +0300

    Config angular to upload files

--- src/AppBundle/Resources/public/js/app.js
+++ src/AppBundle/Resources/public/js/app.js
@@ -5,5 +5,43 @@
         .config(function($interpolateProvider, $httpProvider, $resourceProvider){
             $interpolateProvider.startSymbol('[[').endSymbol(']]');
             $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
-        });
+            $httpProvider.defaults.headers.post['Content-Type'] = undefined;
+            $httpProvider.defaults.transformRequest = function(data) {
+                if (data === undefined)
+                    return data;
+
+                var fd = new FormData();
+                angular.forEach(data, function(value, key) {
+                    if (value instanceof FileList) {
+                        if (value.length == 1) {
+                            fd.append(key, value[0]);
+                        } else {
+                            angular.forEach(value, function(file, index) {
+                                fd.append(key + '_' + index, file);
+                            });
+                        }
+                    } else {
+                        fd.append(key, value);
+                    }
+                });
+
+                return fd;
+            };
+
+
+    });
+
+    //https://github.com/angular/angular.js/issues/1375#issuecomment-21933012
+    app.directive('filesModel', function filesModelDirective(){
+        return {
+            controller: function($parse, $element, $attrs, $scope){
+                var exp = $parse($attrs.filesModel);
+
+                $element.on('change', function(){
+                    exp.assign($scope, this.files);
+                    $scope.$apply();
+                });
+            }
+        };
+    })
 })();
